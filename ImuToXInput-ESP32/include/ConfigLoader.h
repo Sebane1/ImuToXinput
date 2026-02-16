72b62ffbab0bcb296d0ddb69a94155dd24bfbbb3
Sebastina <sebane1@thavea.com>
2026-02-16 17:38:41 -0600

    Remove deprecated code.

--- ImuToXInput-ESP32/include/ConfigLoader.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_LOADER_H
-#define CONFIG_LOADER_H
-
-#include "Config.h"
-
-// Load a GameProfile from a JSON file on LittleFS. Returns true on success.
-bool loadConfig(const char* path, GameProfile* out);
-
-#endif
--- ImuToXInput-ESP32/src/Xbox360Report.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/Xbox360Report.h"
-#include <string.h>
-
-void reportClear(Xbox360Report* r) {
-  if (!r) return;
-  memset(r, 0, sizeof(Xbox360Report));
-}
